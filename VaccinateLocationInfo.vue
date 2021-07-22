2cfaec5c78bed485501f749320a6779490b749de
ZUZU <69393030+zuzu578@users.noreply.github.com>
2021-07-22 11:27:19 +0900

    백신접종센터 정보 vue

---
+++ VaccinateLocationInfo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <h1> VaccinationCorona </h1>
+              <!-- 데이터 동적 binding !-->
+       <div class="board_view_area" v-for="(jData, idx) in boardData" :key="idx">
+           {{jData}}
+
+        </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            boardData:''
+        }
+    },
+    mounted(){
+        axios.get('http://localhost:8082/myapp/VaccinationCorona')
+
+    .then((res)=>{
+        //get Data List 
+        console.log(res.data.data);
+        this.boardData = res.data.data;
+    })
+    },
+    created(){
+
+    },
+    methods:{
+
+    },
+
+}
+</script>
+
+<style>
+
+</style>
